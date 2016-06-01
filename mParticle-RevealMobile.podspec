ccd87bcb6c9781057b0d77531057a114cab43829
Bobby Skinner <bobbyski@users.noreply.github.com>
2016-06-01 17:17:54 -0400

    Implement mParticle Wrapper for RevealMobile

---
+++ mParticle-RevealMobile.podspec
@@ -0,0 +1,21 @@
+Pod::Spec.new do |s|
+    s.name             = "mParticle-RevealMobile"
+    s.version          = "6.10.0"
+    s.summary          = "Reveal Mobile integration for mParticle"
+
+    s.description      = <<-DESC
+                       This is the Reveal Mobile integration for mParticle.
+                       DESC
+
+    s.homepage         = "https://www.mparticle.com"
+    s.license          = { :type => 'Apache 2.0', :file => 'LICENSE' }
+    s.author           = { "mParticle" => "support@mparticle.com", "Reveal Mobile" => "support@revealmobile.com" }
+    s.source           = { :git => "https://github.com/mparticle-integrations/mparticle-apple-integration-revealmobile.git", :tag => s.version.to_s }
+
+    s.ios.deployment_target = "8.0"
+    s.ios.source_files      = 'mParticle-RevealMobile/*.{h,m,mm}'
+    s.ios.dependency 'mParticle-Apple-SDK/mParticle', '~> 6.10.0'
+    s.ios.dependency 'Reveal', '~> 1.3'
+    s.source_files = 'mParticle-RevealMobile/**/*'
+    s.requires_arc = true
+end
---
+++ mParticle-RevealMobile/MPKitRevealMobile.m
@@ -0,0 +1,97 @@
+//
+//  MPKitRevealMobile.m
+//
+//  Copyright 2016 mParticle, Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+#import "MPKitRevealMobile.h"
+#import "mParticle.h"
+#import "Reveal.h"
+
+// // This is temporary to allow compilation (will be provided by core SDK)
+// NSUInteger MPKitInstanceRevealMobile = 112;
+
+@interface MPKitRevealMobile () <RVLBeaconDelegate>
+@property (nonatomic, strong, nullable) Reveal *revealSDK;
+@end
+
+@implementation MPKitRevealMobile
+
+/*
+    mParticle will supply a unique kit code for you. Please contact our team
+*/
++ (NSNumber *)kitCode {
+    return @112;
+}
+
++ (void)load {
+    MPKitRegister *kitRegister = [[MPKitRegister alloc] initWithName:@"RevealMobile" className:@"MPKitRevealMobile" startImmediately:YES];
+    [MParticle registerExtension:kitRegister];
+}
+
+#pragma mark - MPKitInstanceProtocol methods
+
+#pragma mark Kit instance and lifecycle
+- (nonnull instancetype)initWithConfiguration:(nonnull NSDictionary *)configuration startImmediately:(BOOL)startImmediately {
+    self = [super init];
+
+    NSString *appKey = configuration[@"apiKey"];
+    if (self && appKey) {
+        _configuration = configuration;
+
+        if (startImmediately) {
+            [self start];
+        }
+    }
+
+    return self;
+}
+
+- (void)start {
+    static dispatch_once_t kitPredicate;
+
+    dispatch_once(&kitPredicate, ^{
+        NSString *appKey = self.configuration[@"apiKey"];
+        // Get a reference to the SDK object
+        self.revealSDK = [Reveal sharedInstance];
+
+        // Turn on debug logging, not for production
+        self.revealSDK.debug = YES;
+        self.revealSDK.delegate = self;
+
+        [self.revealSDK setupWithAPIKey:appKey andServiceType:RVLServiceTypeProduction];
+
+        _started = YES;
+
+        // Once the config values are set, start the SDK.
+        // The SDK will contact the server for further config info
+        // and start monitoring for beacons.
+        [self.revealSDK start];
+
+        dispatch_async(dispatch_get_main_queue(), ^{
+            NSDictionary *userInfo = @{mParticleKitInstanceKey:[[self class] kitCode]};
+
+            [[NSNotificationCenter defaultCenter] postNotificationName:mParticleKitDidBecomeActiveNotification
+                                                                object:nil
+                                                              userInfo:userInfo];
+        });
+    });
+}
+
+- (id const)providerKitInstance {
+    return [self started] ? self.revealSDK : nil;
+}
+
+@end
